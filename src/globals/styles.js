8ffe880bca6a287738978838afd4106755968402
Zen_99 <2019cs161@stu.ucsc.lk>
2022-07-20 00:55:10 +0530

    Landing page done

---
+++ src/globals/styles.js
@@ -0,0 +1,17 @@
+
+import  {getStatusBarHeight}  from 'react-native-status-bar-height';
+import { Dimensions } from 'react-native';
+export const colors={
+    orange:'#FF8C01',
+    grey:'#28231F',
+    white:'#f2f2f2',
+    font:'#090807',
+    midBoxWhite:'#ffffff'
+}
+
+export const parameters={
+    statusBarHeight :getStatusBarHeight(),
+    headerHeight:90,
+    SCREEN_WIDTH:Dimensions.get('window').width,
+    SCREEN_HEIGHT:Dimensions.get('window').height
+}
