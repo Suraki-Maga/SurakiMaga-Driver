f616efe5de1f21a858c6cbf2429ace2a51c9fa6e
Zen_99 <2019cs161@stu.ucsc.lk>
2022-07-22 14:28:06 +0530

    ProfilePage

--- App.js
+++ App.js
@@ -2,12 +2,13 @@
 import { StyleSheet, Text, View } from 'react-native';
 import LandingPage from './src/screens/LandingPage';
 import Login from './src/screens/Login';
+import ProfilePage from './src/screens/ProfilePage';
 import Registration from './src/screens/Registration';
 import Signup from './src/screens/Signup';
 export default function App() {
   return (
     <View>
-      <Login/>
+      <ProfilePage/>
     </View>
   );
 }
